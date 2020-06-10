3e991100d53020fe61720c24f25a37eccf2871c9
Muhammad Tifak <moht20009@gmail.com>
2020-06-10 02:19:58 -0700

    Proper separation of concerns
    Didn't do all routes, will go back and complete this step later.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override')
 const app = express()
 const bodyParser = require('body-parser');
 const models = require('./db/models');
+require('./controllers/events')(app, models);
 
 // Use "main" as our default layout
 const handlebars = require('handlebars');
@@ -22,21 +23,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
 
-// Index
-app.get('/', (req, res) => {
-  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-    res.render('events-index', { events: events });
-  })
-})
 
-// OUR MOCK ARRAY OF PROJECTS
-var events = [
-  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fbatman-arkham-knight%2FEGS_WB_Batman_Arkham_Knight_G1_1920x1080_19_0911-1920x1080-1d69e15f00cb5ab57249f208f1f8f45d52cbbc59.jpg?h=1080&resize=1&w=1920" },
-  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fbatman-arkham-knight%2FEGS_WB_Batman_Arkham_Knight_G1_1920x1080_19_0911-1920x1080-1d69e15f00cb5ab57249f208f1f8f45d52cbbc59.jpg?h=1080&resize=1&w=1920" },
-  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fbatman-arkham-knight%2FEGS_WB_Batman_Arkham_Knight_G1_1920x1080_19_0911-1920x1080-1d69e15f00cb5ab57249f208f1f8f45d52cbbc59.jpg?h=1080&resize=1&w=1920" }
-]
-
-// INDEX
+// HOME
 app.get('/events', (req, res) => {
   res.render('events-index', { events: events });
 })
